ec195c4cabf2b409e5b8ff9d8266667b3e9b75d9
pathakch <ckp4392@gmail.com>
2024-09-02 23:26:28 +0530

    corrected lambda function

--- lambda_code/lambda_fn.py
+++ lambda_code/lambda_fn.py
@@ -1,5 +1,7 @@
 from datetime import datetime
-current_datetime = datetime.datetime.now()
+current_datetime = datetime.now()
 
 def lambda_fn(event, context):
     print(f"This is the current datetime {current_datetime} in region ap-south-1 ")
+
+lambda_fn(None,None)
